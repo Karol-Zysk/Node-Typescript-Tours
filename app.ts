da4ca281f1c08e0ed231eb0efc26000cfdf786ed
Karol Zyśk <trener.karolzysk@gmail.com>
2022-11-08 11:37:16 +0100

    Heroku

--- app.ts
+++ app.ts
@@ -31,7 +31,7 @@ app.set("view engine", "pug");
 app.set("views", path.resolve(process.cwd(), "src/views"));
 
 //serving static files
-app.use(express.static(path.join(__dirname, "/src/public")));
+app.use(express.static(path.resolve(process.cwd(), "src/public")));
 
 //Set security HTTP headers
 app.use(
