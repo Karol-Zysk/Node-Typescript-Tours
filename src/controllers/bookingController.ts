1a745529d2b96e8750a4973e564c187c6600b3c4
Karol Zyśk <trener.karolzysk@gmail.com>
2022-11-09 11:57:01 +0100

    Heroku

--- src/controllers/bookingController.ts
+++ src/controllers/bookingController.ts
@@ -21,46 +21,32 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 export const getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
-  if (tour) {
-    // 2) Create checkout session
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
-      //   req.params.tourId
-      // }&user=${req.user.id}&price=${tour.price}`,
-      success_url: `${req.protocol}://${req.get(
-        'host'
-      )}/my-tours?alert=booking`,
-      cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour?.slug}`,
-      customer_email: res.locals.user.email,
-      client_reference_id: req.params.tourId,
-      mode: 'payment',
-      line_items: [
-        {
-          price_data: {
-            currency: 'usd',
-            unit_amount: tour?.price ? tour?.price * 100 : 0,
-            product_data: {
-              name: `${tour?.name} Tour`,
-              description: tour?.summary,
-              images: [`https://www.natours.dev/img/tours/${tour?.imageCover}`],
-            },
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour?.slug}`,
+    customer_email: res.locals.user.email,
+    client_reference_id: req.params.tourId,
+    mode: 'payment',
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour?.price ? tour?.price * 100 : 0,
+          product_data: {
+            name: `${tour?.name} Tour`,
+            description: tour?.summary,
+            images: [`https://www.natours.dev/img/tours/${tour?.imageCover}`],
           },
-          quantity: 1,
         },
-      ],
-    });
-    console.log(session + '💥💥💥💥💥💥💥💥💥');
-    // 3) Create session as response
-    res.status(200).json({
-      status: 'success',
-      session,
-    });
-  } else {
-    res.status(404).json({
-      status: 'fail',
-    });
-  }
+        quantity: 1,
+      },
+    ],
+  });
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
 });
 
 const createBookingCheckout = async (session: any) => {
